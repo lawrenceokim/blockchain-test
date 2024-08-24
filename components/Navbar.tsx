461a2e562331443c48d8b519f0096702f5aabf18
lawrenceokim <lawrence4okim@gmail.com>
2024-08-24 12:08:50 +0100

    home ui

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="fixed top-0 left-1/2 -translate-x-1/2 max-w-[1000px] w-full py-5 mt-3 rounded-3xl bg-blue-800">
+      Navbar
+    </nav>
+  );
+}
+
+export default Navbar;
